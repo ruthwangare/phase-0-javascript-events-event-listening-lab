8044ee3438a2ffb98fdd43d69b2e00d00aa8163e
Ruth <rwangare124@gmail.com>
2023-03-15 15:36:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-function addingEventListener() {
+function addingEventListener(){
+  console.log('inside eventlistener');
+  const input = document.getElementById('main');
+  input.addEventListener('click', clickAlert());
 }
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+// addingEventListener()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
